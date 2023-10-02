9cd69990265ff51a89e949b8ad412878fac9bd3a
ada aykol <2031665@brunel.ac.uk>
2023-10-02 13:03:42 +0100

    added the keyboard functionalities to test and se how to connect it to typing

--- Assets/Scripts/Handler.cs
+++ Assets/Scripts/Handler.cs
@@ -5,15 +5,5 @@ using UnityEngine.UI;
 
 public class Handler : MonoBehaviour
 {
-    public string buttonInput = string.Empty;
-    public Button button; 
 
-    private void Awake(){
-        button.onClick.AddListener(SubmitLetter);
-    }
-
-    private string SubmitLetter(){
-        
-        return buttonInput;
-    }
 }
---
+++ Assets/Scripts/keyboard.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class keyboard : MonoBehaviour
+{
+    string word = null;
+    int wordIndex = 0;
+    string alpha;
+    public Text myName= null;
+
+    public void alphabetFunction(string alphabet){
+        wordIndex++;
+        word = word + alphabet;
+        myName.text = word;    
+    }
+}
